9f269dfd6b3ccac95079611b2ba5de81601dbb28
BuildTools <unconfigured@null.spigotmc.org>
2016-08-13 02:03:37 +0100

    stuff

--- src/main/java/me/martinitslinda/infected/event/GameEndEvent.java
+++ src/main/java/me/martinitslinda/infected/event/GameEndEvent.java
@@ -1,19 +1,9 @@
 package me.martinitslinda.infected.event;
 
 import org.bukkit.event.Event;
-import org.bukkit.event.HandlerList;
 
 public class GameEndEvent extends Event{
 
-    private static final HandlerList handlers=new HandlerList();
 
-    public static HandlerList getHandlerList(){
-        return handlers;
-    }
-
-    @Override
-    public HandlerList getHandlers(){
-        return handlers;
-    }
 
 }
